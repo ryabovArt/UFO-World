ec1146f6166c2366f355e5f0df71d3d0cac6eec4
ryabovArt <79091451+ryabovArt@users.noreply.github.com>
2021-05-19 16:08:35 +0300

    Bug fix

---
+++ Assets/Scripts/UFO/LevelComplete.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelComplete : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out UFOMovement movement))
+        {
+            print("WIN");
+        }
+    }
+}
